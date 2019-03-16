222b4609e0dc3cba7eeaf138ebeb7a1894f147c0
Fraz Ikram <frazikram1@gmail.com>
2019-03-16 12:16:18 -0400

    Inital commit

---
+++ os.c
@@ -0,0 +1,12 @@
+//First letinclude all the required system libraries.
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <assert.h>
+
+//Global Variables
+
+long RQ;
+long OSFreeList;
+long UserFreeList;
